cdcabe81e96f851b712f94517db37695653611e2
Juraj Cirbus <juraj.cirbus@norconsult.com>
2023-10-20 16:03:01 +0200

    - test map on sides

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LazyElementsModule } from '@angular-extensions/elements';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { Side1Component } from './side1/side1.component';
+import { Side2Component } from './side2/side2.component';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MapComponent, Side1Component, Side2Component],
   imports: [BrowserModule, LazyElementsModule],
   providers: [],
   bootstrap: [AppComponent],
